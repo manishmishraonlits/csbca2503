b61dd41cb99d3cbb827ef4b7443345871680ae43
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2025-04-21 13:59:55 +0530

    20250421

---
+++ java/nopackage/myexercise/projectsynopsys/ProjectSynopsis.java
@@ -0,0 +1,14 @@
+class ProjectSynopsis {
+    private String title;
+    private String problemStatement;
+    private String[] objectives;
+    private String scope;
+
+    public void read() {
+
+    }
+
+    public void display() {
+        
+    }
+}
