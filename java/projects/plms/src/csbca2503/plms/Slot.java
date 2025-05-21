04eda987adf1314a3ac9c8b49c637c6c8b05e7a1
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2025-05-21 13:19:12 +0530

    20250521

--- java/projects/plms/src/csbca2503/plms/Slot.java
+++ java/projects/plms/src/csbca2503/plms/Slot.java
@@ -1,6 +1,9 @@
 package csbca2503.plms;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Slot {
 
@@ -23,8 +26,15 @@ public class Slot {
         return this;
     }
 
-    void save() {
-        Connection connection = new Connection();
-    
+    void save() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/plms_2503";
+        String user = "csbca2503";
+        String password = "csbca2503";
+        Connection connection = DriverManager.getConnection(url, user, password);
+        String sql = "INSERT INTO slot(id, type) VALUES('" + id + "', '" + type + "');";
+        Statement statement = connection.createStatement();
+        statement.execute(sql);
+        connection.close();
+        System.out.println("Data inserted!");
     }
 }
--- java/projects/plms/src/csbca2503/plms/TestSlot.java
+++ java/projects/plms/src/csbca2503/plms/TestSlot.java
@@ -1,8 +1,11 @@
 package csbca2503.plms;
 
+import java.sql.SQLException;
+
 class TestSlot {
-    public static void main(String[] args) {
-        Slot s1 = new Slot("S001", "Car");
+    public static void main(String[] args) throws SQLException {
+        Slot s1 = new Slot("S003", "4W");
         s1.getSlot();
+        s1.save();
     }
 }
