8b2a17faa2a0a1c04254336fba6aa06dd44b60b3
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2025-05-22 13:13:23 +0530

    20250522

--- java/projects/plms/src/csbca2503/plms/Slot.java
+++ java/projects/plms/src/csbca2503/plms/Slot.java
@@ -2,6 +2,7 @@ package csbca2503.plms;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -18,23 +19,51 @@ public class Slot {
         this.type = type;
     }
 
+    public String getId() {
+        return id;
+    }
+
     public void setType(String type) {
         this.type = type;
+    }
+
+    public String getType() {
+        return type;
     }
 
     public Slot getSlot() {
         return this;
     }
 
-    void save() throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/plms_2503";
-        String user = "csbca2503";
-        String password = "csbca2503";
+    public void save() throws SQLException {
+        final String url = "jdbc:mysql://localhost:3306/plms_2503";
+        final String user = "csbca2503";
+        final String password = "csbca2503";
         Connection connection = DriverManager.getConnection(url, user, password);
         String sql = "INSERT INTO slot(id, type) VALUES('" + id + "', '" + type + "');";
         Statement statement = connection.createStatement();
-        statement.execute(sql);
+        statement.executeUpdate(sql);
         connection.close();
         System.out.println("Data inserted!");
     }
+
+    public static Slot[] getSlots() throws SQLException {
+        Slot[] slots = new Slot[10];            // Replace it by ArrayList instance
+        final String url = "jdbc:mysql://localhost:3306/plms_2503";
+        final String user = "csbca2503";
+        final String password = "csbca2503";
+        Connection connection = DriverManager.getConnection(url, user, password);
+        String sql = "SELECT * FROM slot";
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+        int i = 0;
+        while(resultSet.next()){
+            slots[i].id = resultSet.getString("id");
+            slots[i].type = resultSet.getString("type");
+            i++;
+        }
+        connection.close();
+        return slots;
+    }
+
 }
--- java/projects/plms/src/csbca2503/plms/TestSlot.java
+++ java/projects/plms/src/csbca2503/plms/TestSlot.java
@@ -4,8 +4,18 @@ import java.sql.SQLException;
 
 class TestSlot {
     public static void main(String[] args) throws SQLException {
-        Slot s1 = new Slot("S003", "4W");
-        s1.getSlot();
-        s1.save();
+        Slot[] slots = Slot.getSlots();
+        // displaySlots(slots);
+        // Slot s1 = new Slot("S003", "4W");
+        // displaySlots(slots);
+        // s1.save();
+    }
+
+    public static void displaySlots(Slot[] slots) {
+        System.out.printf("Id\tType\n");
+        System.out.printf("------------------------------------\n");
+        for (Slot slot : slots) {
+            System.out.printf("%-10s%-30s", slot.getId(), slot.getType());
+        }
     }
 }
